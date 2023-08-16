0d0b108868449b2ca91642892fa62cd91cdd2a48
DESKTOP-8857DGO\503 <audelahd@gmail.com>
2023-08-16 14:02:39 +0900

    Gaya_front 수정

--- src/views/memberinfo/sections/adminres.jsx
+++ src/views/memberinfo/sections/adminres.jsx
@@ -40,7 +40,9 @@ const Adminres = () => {
 
   async function fetchData() {
     try {
-      const response = await fetch(process.env.REACT_APP_SERVER_LOCAL + "/admin/reservation");
+      const response = await fetch(
+        process.env.REACT_APP_SERVER_LOCAL + "/admin/reservation"
+      );
       const data = await response.json();
       setReservation(data);
     } catch (error) {
@@ -52,7 +54,9 @@ const Adminres = () => {
     return Object.keys(options)
       .filter((key) => key.startsWith("imt"))
       .map((optionKey) => (
-        <Col md="6" key={optionKey}>{options[optionKey].option_content}</Col>
+        <Col md="6" key={optionKey}>
+          {options[optionKey].option_content}
+        </Col>
       ));
   };
   const customStyles2 = {
@@ -71,101 +75,111 @@ const Adminres = () => {
   const RenderReservationRows = () => {
     if (totReservation.length > 0) {
       return totReservation.map((Res, index) => (
-        <Row key={index} style={{ textAlign: "center", height: "70px", marginTop:"15px" }}>
-          <hr style={{margin:"0 auto", width:"97%", marginBottom:"7px"}}/>
-          <Col style={{lineHeight:"70px"}} >{Res.res_num.reservation_num}</Col>
-          <Col md="3" style={{display:"table"}}>
-            <div style={{display: "table-cell",verticalAlign: "middle"}}>
+        <Row
+          key={index}
+          style={{ textAlign: "center", height: "70px", marginTop: "15px" }}
+        >
+          <hr style={{ margin: "0 auto", width: "97%", marginBottom: "7px" }} />
+          <Col style={{ lineHeight: "70px" }}>
+            {Res.res_num.reservation_num}
+          </Col>
+          <Col md="3" style={{ display: "table" }}>
+            <div style={{ display: "table-cell", verticalAlign: "middle" }}>
               {Res.res_num.id.id}
               <br />
               {Res.res_num.order_id}
             </div>
           </Col>
-          <Col md="2" style={{display:"table"}}>
-            <div style={{display: "table-cell",verticalAlign: "middle"}}>
+          <Col md="2" style={{ display: "table" }}>
+            <div style={{ display: "table-cell", verticalAlign: "middle" }}>
               {Res.res_num.r_num.r_num}
               <br />
               {Res.res_num.r_num.r_type}
             </div>
           </Col>
-          <Col style={{display:"table"}}>
-            <div style={{display: "table-cell",verticalAlign: "middle"}}>
+          <Col style={{ display: "table" }}>
+            <div style={{ display: "table-cell", verticalAlign: "middle" }}>
               {new Date(Res.res_num.check_in).toLocaleDateString()}
               <br />~ {new Date(Res.res_num.check_out).toLocaleDateString()}
             </div>
           </Col>
-          <Col style={{display:"table", lineHeight:'70px'}}>
-          <input
-                type="button"
-                className="btn btn-primary"
-                id={index}
-                onClick={btn_togle2}
-                value={"선택 옵션 보기"}
-                style={{
-                  display: "table-cell",
-                  verticalAlign: "middle",
-                  height: "44",
-                  fontSize:"13px",
-                  padding:"10px"
-                }}
-              ></input>
+          <Col style={{ display: "table", lineHeight: "70px" }}>
+            <input
+              type="button"
+              className="btn btn-primary"
+              id={index}
+              onClick={btn_togle2}
+              value={"선택 옵션 보기"}
+              style={{
+                display: "table-cell",
+                verticalAlign: "middle",
+                height: "44",
+                fontSize: "13px",
+                padding: "10px",
+              }}
+            ></input>
           </Col>
 
-          <Col style={{display:"table"}}>
-            <div style={{display: "table-cell",verticalAlign: "middle"}}>
+          <Col style={{ display: "table" }}>
+            <div style={{ display: "table-cell", verticalAlign: "middle" }}>
               {Res.res_num.total_price}
             </div>
           </Col>
 
           <Modal id={index} style={customStyles2} isOpen={modelon2[index]}>
-                <Container>
-                  <Row style={{ height: "30px", margin: "10px 0 20px 0"}}>
-                    <Col
-                      style={{
-                        fontSize: "25px",
-                        textAlign: "center",
-                        fontFamily: "Orbit",
-                      }}
-                    >
-                      선택된 옵션
-                    </Col>
-                  </Row>
-                  <Row style={{ textAlign: "center" }}>
-                    {(()=>{
-                      if(Res.length === 1){
-                        return <Col style={{margin:"15px"}}>선택된 옵션이 없습니다</Col>;
-                      }else{
-                        return Object.keys(Res)
-                        .filter((key) => key.startsWith("imt"))
-                        .map((optionKey) => (
-                            <Col md="6" key={optionKey}>
-                              <hr/>
-                              {Res[optionKey].option_content}
-                              <hr/>
-                            </Col>
-                        ));
-                      }
-                    })()}
-                  </Row>
-                  <Row>
-                    <Col style={{ textAlign: "center", margin: "0 0 15px 0" }}>
-                      <input
-                        type="button"
-                        value={"닫기"}
-                        onClick={btn_togle2}
-                        className="btn btn-secondary"
-                      />
-                    </Col>
-                  </Row>
-                </Container>
-              </Modal>
+            <Container>
+              <Row style={{ height: "30px", margin: "10px 0 20px 0" }}>
+                <Col
+                  style={{
+                    fontSize: "25px",
+                    textAlign: "center",
+                    fontFamily: "Orbit",
+                  }}
+                >
+                  선택된 옵션
+                </Col>
+              </Row>
+              <Row style={{ textAlign: "center" }}>
+                {(() => {
+                  if (Res.length === 1) {
+                    return (
+                      <Col style={{ margin: "15px" }}>
+                        선택된 옵션이 없습니다
+                      </Col>
+                    );
+                  } else {
+                    return Object.keys(Res)
+                      .filter((key) => key.startsWith("imt"))
+                      .map((optionKey) => (
+                        <Col md="6" key={optionKey}>
+                          <hr />
+                          {Res[optionKey].option_content}
+                          <hr />
+                        </Col>
+                      ));
+                  }
+                })()}
+              </Row>
+              <Row>
+                <Col style={{ textAlign: "center", margin: "0 0 15px 0" }}>
+                  <input
+                    type="button"
+                    value={"닫기"}
+                    onClick={btn_togle2}
+                    className="btn btn-secondary"
+                  />
+                </Col>
+              </Row>
+            </Container>
+          </Modal>
         </Row>
       ));
     } else {
       return (
         <Row>
+          <hr />
           <Col></Col>
-          <Col>예약된 내역이 없습니다.</Col>
+          <Col style={{ textAlign: "center" }}>예약된 내역이 없습니다.</Col>
           <Col></Col>
         </Row>
       );
@@ -176,16 +190,19 @@ const Adminres = () => {
     <>
       <Container style={{ marginBottom: "200px" }}>
         <Row>
-          <Col style={{
-            fontSize: "40px",
-            fontFamily: "Orbit",
-            margin: "30px 0 30px 20px",
-            textAlign:"center"
-          }}
-        >예약내역</Col>
+          <Col
+            style={{
+              fontSize: "40px",
+              fontFamily: "Orbit",
+              margin: "30px 0 30px 20px",
+              textAlign: "center",
+            }}
+          >
+            예약내역
+          </Col>
         </Row>
-        <hr />
-        <Row style={{ textAlign: "center", lineHeight: "40px" }}>
+        <hr style={{ margin: "0" }} />
+        <Row style={{ textAlign: "center", lineHeight: "60px" }}>
           <Col>예약번호</Col>
           {/* 아이디랑 order_idZ */}
           <Col md="3">예약정보</Col>
@@ -194,14 +211,11 @@ const Adminres = () => {
           <Col>옵션</Col>
           <Col>총결제금액</Col>
         </Row>
-        
+
         <RenderReservationRows />
         <hr />
         <Adminprice />
       </Container>
-
-      
-
     </>
   );
 };
